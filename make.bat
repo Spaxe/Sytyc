47ac3fce5277f2e74d2846e6e5c3dddb396fcf49
Xavier Ho <contact@xavierho.com>
2011-06-11 23:32:33 +1000

    Modified make.bat to reflect the change of filenames in the last commit

--- make.bat
+++ make.bat
@@ -3,14 +3,14 @@ cd src
 echo "Compiling Sytyc ..."
 ghc --make -O2 -H14m index.hs
 ghc --make -O2 -H14m problem.hs
-ghc --make -O2 -H14m problems.hs
+ghc --make -O2 -H14m problem_list.hs
 ghc --make -O2 -H14m faq.hs
 ghc --make -O2 -H14m feedback.hs
 echo "Copying executables ..."
 cd ..
 mv src/index.exe index.cgi
 mv src/problem.exe problem.cgi
-mv src/problems.exe problems.cgi
+mv src/problem_list.exe problem_list.cgi
 mv src/feedback.exe feedback.cgi
 mv src/faq.exe faq.cgi
 echo "Cleaning up ..."
